71764c8dcf10fda81356b9a7f7126dedf5833d66
joaoguibc <joaoguibc@gmail.com>
2022-09-20 00:22:14 -0300

    Feat: Create header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import type { AppProps } from 'next/app';
+import Image from 'next/future/image';
+
+import logoImg from '../assets/logo.svg';
 
 import { globalStyles } from '../styles/globals';
+import { Container, Header } from '../styles/pages/app';
 
 globalStyles();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="Base store logo" height={120} width={120} />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
