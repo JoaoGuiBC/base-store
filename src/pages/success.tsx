fd82a44ec8b97043db16588ed6a0e5879c45dd40
joaoguibc <joaoguibc@gmail.com>
2022-09-22 11:59:31 -0300

    Feat: Success page can only be accessed after a purchase

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -57,6 +57,15 @@ const Success: NextPage<SuccessProps> = ({ customerName, product }) => {
 export default Success;
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
   const sessionId = String(query.session_id);
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
